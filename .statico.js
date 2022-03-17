d8d9655a9c36aa640fb91d2cacc1e508f94b996c
GordonAnsell <moving@ga1964.com>
2022-03-17 05:56:23 +0000

    Update .statico.js

--- .statico.js
+++ .statico.js
@@ -8,6 +8,7 @@
 
 const { syslog } = require('js-framework');
 const RatingStarsShortcode = require('./src/shortcodes/ratingstarsShortcode');
+const pack = require('./package.json')
 const debug = require('debug')('Statico:plugin:ratingstars'),
       debugf = require('debug')('Full.Statico:plugin:ratingstars');
 
@@ -16,5 +17,8 @@ module.exports = function(config, options = {}) {
 
     config.addNunjucksShortcode('ratingstars', RatingStarsShortcode);
     debug(`Added shortcode to Nunjucks: ratingstars`);
+
+    syslog.notice(`Statico rating stars plugin version ${pack.version} loaded.`);
+
 
 }
